428b08e440b298fc2ad8985e196e85b46a3f2380
German Aviles <german.aviles06@gmail.com>
2021-10-21 21:54:02 -0500

    Ejercicio #2 terminado

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
 
 
 function main() {
-  console.log('=====================================================');
-  console.log('Ejercicio #1');
-  const listaNumeros = [ 9, 10, 20, 20, 10, 10, 30, 50, 10, 20 ];
-  const paresRepetidos = contarNumerosParesRepetidos( listaNumeros );
+
+  console.log('================= Ejercicio #1 =================');
+  const listaNumerosEjercicio1 = [ 9, 10, 20, 20, 10, 10, 30, 50, 10, 20 ];
+  const paresRepetidos = contarNumerosParesRepetidos( listaNumerosEjercicio1 );
   console.log('Numero de pares repetidos: ', paresRepetidos);
-  console.log('=====================================================');
+  console.log('================================================');
+  console.log('');
+
+  console.log('================= Ejercicio #2 =================');
+  const listaNumerosEjercicio2 = [ 2, 3, 5, 1, 4 ];
+  const { sumaMinima, sumaMaxima } = sumaMaximosMinimos( listaNumerosEjercicio2 );
+  console.log(`Suma minima: ${ sumaMinima }  Suma máxima: ${ sumaMaxima }`);
+  console.log('================================================');
 }
 
 function contarNumerosParesRepetidos( listaNumeros ) {
   let numeroCoincidencias = 0;
 
-  if ( Array.isArray( listaNumeros ) ){
-
-    const listaSoloNumeros = listaNumeros.every( (numero) => isNaN( numero ) === false  );
-
-    if (listaSoloNumeros) {
-      const datos = new Set( listaNumeros );
-      const listaLimpia = [...datos];
+  if ( esUnaListaNumericaValida( listaNumeros ) ){
+      const datosUnicos = new Set( listaNumeros );
+      const listaLimpia = [...datosUnicos];
 
       listaLimpia.forEach( (numero) => {
         const coincidencias = listaNumeros.filter( (number) => number === numero);
@@ -30,11 +33,38 @@ function contarNumerosParesRepetidos( listaNumeros ) {
 
       });
       return numeroCoincidencias;
-    }
-
   }
   console.error('No es una lista valida, vuelve a intentarlo');
   return numeroCoincidencias;
+}
+
+function sumaMaximosMinimos( listaNumeros ) {
+  let sumaMinima = 0;
+  let sumaMaxima = 0;
+
+  if ( esUnaListaNumericaValida( listaNumeros ) ) {
+    const minimo = Math.min( ...listaNumeros );
+    const maximo = Math.max( ...listaNumeros );
+
+    sumaMinima = listaNumeros.reduce( (acum, actual) => acum += actual !== minimo ? actual : 0, 0);
+    sumaMaxima = listaNumeros.reduce( (acum, actual) => acum += actual !== maximo ? actual : 0, 0);
+  } else {
+    console.error('Ingrese una lista valida');
+  }
+
+  return { sumaMinima, sumaMaxima };
+}
+
+function esUnaListaNumericaValida( listado ) {
+
+  let valida = false;
+
+  if ( Array.isArray(listado) ) {
+    valida = listado.every( (numero) => isNaN( numero ) === false  );
+  }
+
+  return valida;
+
 }
 
 main();
