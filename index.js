7eea9f938ce96a79b31112b24bd168872291815a
German Aviles <german.aviles06@gmail.com>
2021-10-21 21:21:21 -0500

    ejercicio coincidencias de pares listo

--- index.js
+++ index.js
@@ -1,16 +1,40 @@
 
 
 function main() {
-  obtenerCoincidencia(1);
+  console.log('=====================================================');
+  console.log('Ejercicio #1');
+  const listaNumeros = [ 9, 10, 20, 20, 10, 10, 30, 50, 10, 20 ];
+  const paresRepetidos = contarNumerosParesRepetidos( listaNumeros );
+  console.log('Numero de pares repetidos: ', paresRepetidos);
+  console.log('=====================================================');
 }
 
-function obtenerCoincidencia( listaNumeros ) {
+function contarNumerosParesRepetidos( listaNumeros ) {
+  let numeroCoincidencias = 0;
+
   if ( Array.isArray( listaNumeros ) ){
-    console.log('TODO GOOD: ', listaNumeros);
-  } else {
-    console.error( 'UN error' );
-    throw "ERROR";
+
+    const listaSoloNumeros = listaNumeros.every( (numero) => isNaN( numero ) === false  );
+
+    if (listaSoloNumeros) {
+      const datos = new Set( listaNumeros );
+      const listaLimpia = [...datos];
+
+      listaLimpia.forEach( (numero) => {
+        const coincidencias = listaNumeros.filter( (number) => number === numero);
+        const sizeArray = coincidencias.length;
+
+        if ( coincidencias.length >= 2 ) {
+          numeroCoincidencias += sizeArray % 2 === 0 ? (sizeArray / 2) : (sizeArray - 1) / 2;
+        }
+
+      });
+      return numeroCoincidencias;
+    }
+
   }
+  console.error('No es una lista valida, vuelve a intentarlo');
+  return numeroCoincidencias;
 }
 
 main();
